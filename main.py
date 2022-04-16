da5ae01bfd721f8e9969bf8b146a16b22cfa2831
Krina1103 <kpatel107@horizon.csueastbay.edu>
2022-04-16 15:53:29 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+"""
+Python Data Structures - A Game-Based Approach
+Stack challenge
+Robin Andrews - https://compucademy.net/
+"""
+
+import stack
+
+string = "gninraeL nIdekniL htiw tol a nraeL"
+reversed_list = []
+reversed_string = ""
+s = stack.Stack()
+for i in string:
+    s.push(i)
+
+while not s.is_empty():
+    reversed_list.append(s.pop())
+
+reversed_string = (''.join(reversed_list))
+print(reversed_string)
+# Your solution here.
+
+
